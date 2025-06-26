e700eda5d7a2a1583839c55c4907a3e21d2203ac
lichengxi <licx199@gmail.com>
2025-06-26 22:08:23 +0800

    chore(gitignore): 更新配置

--- crawler.py
+++ crawler.py
@@ -89,30 +89,5 @@ def get_artist_detail(url, headers=None, cookies=None):
 
 
 if __name__ == "__main__":
-    # url = urljoin(base_url, "discover/artist/cat?id=1001")
-    # base_headers = {
-    #     "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-    #     "accept-language": "zh-CN,zh;q=0.9,en-GB;q=0.8,en-US;q=0.7,en;q=0.6",
-    #     "priority": "u=0, i",
-    #     "referer": "https://music.163.com/",
-    #     "sec-ch-ua": '"Google Chrome";v="137", "Chromium";v="137", "Not/A)Brand";v="24"',
-    #     "sec-ch-ua-mobile": "?0",
-    #     "sec-ch-ua-platform": '"Windows"',
-    #     "sec-fetch-dest": "iframe",
-    #     "sec-fetch-mode": "navigate",
-    #     "sec-fetch-site": "same-origin",
-    #     "upgrade-insecure-requests": "1",
-    #     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36",
-    # }
-    # # 测试函数1：获取歌手id
-    # artists_data = get_artist_id(url, base_headers)
-    # # artists_data.to_csv('artists.csv', index=False)
-    # # 测试函数2：获取歌手详情页信息
-    # with open('cookie.txt','r') as f:
-    #     cookie_str = f.read()
-    # cookies = parse_cookies(cookie_str)
-    # artist_infomation, songs_urls = get_artist_detail('https://music.163.com/artist?id=3684', headers=base_headers, cookies=cookies)
 
-    # print(artist_infomation)
-    # songs_urls.to_csv('songs_urls.csv', index=False)
     print(0)
